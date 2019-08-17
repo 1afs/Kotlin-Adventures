28844eddb796994b7456e87295c96a5c548c8cf7
Andre Fagundes <1afs.andre@gmail.com>
2019-08-17 18:52:01 -0300

    Reciclagem de uma pasta 'res'

--- FinancasK/app/src/main/java/com/andre/financask/ui/activity/ListaTransacoesActivity.kt
+++ FinancasK/app/src/main/java/com/andre/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,2 +1,12 @@
 package com.andre.financask.ui.activity
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ListaTransacoesActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
